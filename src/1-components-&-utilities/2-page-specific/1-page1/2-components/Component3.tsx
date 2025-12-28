bdc36de9131febbedc3e85c7f0e95bc4ec35de13
User User1 <tobyhogan44@gmail.com>
2025-12-28 11:29:36 +0000

    version 1.2.5

--- src/1-components-&-utilities/2-page-specific/1-page1/2-components/Component3.tsx
+++ src/1-components-&-utilities/2-page-specific/1-page1/2-components/Component3.tsx
@@ -13,10 +13,14 @@ const ReactClockComponent = () => {
     };
   }, []);
 
+  /*
+  <h2 className="text-lg font-semibold mb-3 dark:text-white text-center">React Clock Package</h2>
+
+  */
+
   return (
     <div className="flex flex-col items-center justify-center py-8">
       <div className="bg-neutral-100 dark:bg-neutral-800 p-6 rounded-lg">
-        <h2 className="text-lg font-semibold mb-3 dark:text-white text-center">React Clock Package</h2>
         <div className="flex justify-center">
           <div className="bg-white rounded-full p-2 inline-block">
             <Clock value={value} size={200} renderNumbers={true} />
