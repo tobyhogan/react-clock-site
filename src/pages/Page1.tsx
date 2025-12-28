8f6ea3221563b5966826361f343c1aed9002e9ab
User User1 <tobyhogan44@gmail.com>
2025-12-28 11:29:11 +0000

    version 1.2.4

--- src/pages/Page1.tsx
+++ src/pages/Page1.tsx
@@ -1,13 +1,28 @@
 import Clock from '../1-components-&-utilities/2-page-specific/1-page1/2-components/Component1';
-import AnalogClock from '../1-components-&-utilities/2-page-specific/1-page1/2-components/Component2';
+//import AnalogClock from '../1-components-&-utilities/2-page-specific/1-page1/2-components/Component2';
 import ReactClockComponent from '../1-components-&-utilities/2-page-specific/1-page1/2-components/Component3';
+
 
 const Page1 = () => {
   return (
     <div className="container mx-auto px-4 py-8">
+
+
       <Clock />
-      <AnalogClock />
+      
+
+      {/*
+
+        <AnalogClock />
+
+      */}
+
+
+
+
       <ReactClockComponent />
+
+
     </div>
   );
 };
